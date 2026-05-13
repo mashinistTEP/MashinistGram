ad2fade0e47fc5afa7f3e5e79b631f2de5925aa6
mashinistTEP <kirillfillpromortis@gmail.com>
2026-05-13 13:59:10 +0300

    Update MashinistStarsActivity.java

--- TMessagesProj/src/main/java/org/telegram/mashinist/MashinistStarsActivity.java
+++ TMessagesProj/src/main/java/org/telegram/mashinist/MashinistStarsActivity.java
@@ -3,6 +3,7 @@ package org.telegram.mashinist;
 import android.content.Context;
 import android.graphics.Color;
 import android.view.Gravity;
+import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import org.telegram.ui.ActionBar.ActionBar;
